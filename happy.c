ccc2e8ff24ae89e2eab25cbd67cf8a348a1965ab
Calista Mayer <calistammayer@gmail.com>
2023-09-09 17:17:46 -0400

    wip

--- happy.c
+++ happy.c
@@ -9,19 +9,43 @@ int main()
     scanf("%d", &n);
 
     int m = n;
-    int sum_square = 0;
+
+    // keep track of repeating numbers
+    int seen[1000] = {0};
+
     // TODO
     // add code below
-    while (n > 0)
+    while (1)
     {
-        int digit = n % 10;
-        sum_square += digit * digit;
-        digit /= 10;
-    }
+        int sum_square = 0;
 
-    if (n == 1)
-        printf("%d is a happy number.\n", m);
-    else
-        printf("%d is NOT a happy number.\n", m);
+        // sum of squares of all digits
+        while (n > 0)
+        {
+            int digit = n % 10;
+            sum_square += digit * digit;
+            n /= 10;
+        }
+
+        if (sum_square == 1)
+        {
+            printf("%d is a happy number.\n", m);
+            break;
+        }
+        else if (seen[sum_square])
+        {
+            printf("%d is NOT a happy number.\n", m);
+            break;
+        }
+        else
+        {
+            seen[sum_square] = 1;
+            n = sum_square;
+        }
+        // if (n == 1)
+        //     printf("%d is a happy number.\n", m);
+        // else
+        //     printf("%d is a NOT a happy number.\n", m);
+    }
     return 0;
 }
