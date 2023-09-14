4f51cbabd20ccf4435495912ffd93f02cbfc474f
Calista Mayer <calistammayer@gmail.com>
2023-09-14 14:19:32 -0400

    small changes

--- cft/taxicab-number.c
+++ cft/taxicab-number.c
@@ -8,39 +8,33 @@
 //  Once we are done, we will be delighted surprised to find out that this number already played a big role in
 //  our computer science study.
 #include <stdio.h>
-
 int main()
 {
-    int n;
-    printf("Enter number: ");
-    scanf("%d", &n);
-
-    int found = 0;
-    int i, j;
+    int i, j, n;
 
     // We assume the smallest such n is no more than 1000000
-    for (int n = 1; n <= 1000000 && !found; n++)
+    for (n = 1; n <= 1000000; n++)
     {
+        // Use count to record the number of different ways of summing two positive cube numbers to n
         int count = 0;
-
-        for (i = 1; i <= n && !count; i++)
+        // TO DO
+        // Fill in code below
+        // int k, m; // store i and j for the first pair
+        for (i = 1; i <= 100; i++)
         {
-            for (j = i; j <= n && !count; j++)
+            for (j = i; j <= 100; j++)
             {
-                int first_cube = i * i * i;
-                int second_cube = j * j * j;
-
-                if (first_cube + second_cube == n && i != j)
+                if ((i * i * i) + (j * j * j) == n)
                 {
-                    count = 1;
-                    // printf("%d is the smallest positive integer that can be expressed as:\n", n);
-                    // printf("%d * %d * %d + %d * %d * %d = %d\n", i, i, i, j, j, j, n);
-                    found = 1;
+                    count++;
                 }
             }
         }
+        if (count == 2)
+        {
+            break;
+        }
     }
-
     // Do not change code below
     printf("%d is the solution:\n", n);
     for (i = 1; i <= 100; i++)
--- homework1/happy.c
+++ homework1/happy.c
@@ -10,7 +10,7 @@ int main()
 
     int m = n;
 
-    // keep track of repeating numbers
+    // // keep track of repeating numbers
     int seen[1000] = {0};
 
     // TODO
@@ -26,13 +26,14 @@ int main()
             sum_square += digit * digit;
             n /= 10;
         }
+        printf("%d\n", sum_square);
 
         if (sum_square == 1)
         {
             printf("%d is a happy number.\n", m);
             break;
         }
-        else if (seen[sum_square])
+        else if (sum_square == 4)
         {
             printf("%d is NOT a happy number.\n", m);
             break;
@@ -42,10 +43,6 @@ int main()
             seen[sum_square] = 1;
             n = sum_square;
         }
-        // if (n == 1)
-        //     printf("%d is a happy number.\n", m);
-        // else
-        //     printf("%d is a NOT a happy number.\n", m);
     }
     return 0;
 }
